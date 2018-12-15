d93f1efd2637a4274cec903acd3deff3fcf533c9
Nesmikkuma <Nesmith05@yahoo.com>
2018-12-15 08:35:01 -0500

    update

---
+++ test.js
@@ -0,0 +1,10 @@
+var stuff = [10,34,56,67,93,120,137,168,259,280,311,342,413,514];
+
+var random_value = stuff[ Math.floor( Math.random() * 14 ) ];
+
+//benchmarking - tells us how long something takes to do -- look up =how the fuck this works
+//visualgo.net
+//time complexity - how it manages the time
+//linear search always takes longer than binary search
+//binary search is useful with a sorted array
+//interviewbit.com
